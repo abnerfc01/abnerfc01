630144f294e213302b48585d95755ced7cea99a0
Abner Correa <abnerfc01@gmail.com>
2021-04-17 18:23:31 -0300

    Desafios Aula 1 concluidos

--- aula01/index.js
+++ aula01/index.js
@@ -1,13 +1,70 @@
 // Desafios dessa aula!
-// Criar um programa que converta libra em dolar, ou graus Celsius para Fahrenheit;
-// Exibir o resultado da conversão na tela, no lugar da função alert.
+// OK - Criar um programa que converta libra em dolar, ou graus Celsius para Fahrenheit;
+// OK - Exibir o resultado da conversão na tela, no lugar da função alert.
 
-var valorEmDolarTexto = prompt("Qual o valor em dolar que você quer converter?")
+let cotacaoDolarReal = 5.62
 
-var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+let cotacaoLibraDolar = 1.38
 
-var valorEmReal = valorEmDolarNumero * 5.5
+let cotacaoLibraReal = 7.77
 
-var valorEmRealDecimal = valorEmReal.toFixed(2)
+var cotacao = 0
+var moeda = ""
+var moedaConvertida = ""
 
-alert(valorEmRealDecimal)
+
+var opcaoConversao = prompt("Digite a opção de conversão desejada?\n1 - Dolar em Real \n2 - Libra Esterlina em Real \n3 - Libra em Dolar")
+
+if (opcaoConversao == 1) {
+     cotacao = cotacaoDolarReal
+     moeda = "Dolar"
+     moedaConvertida = "reais"
+    
+} else if (opcaoConversao == 2){
+        cotacao = cotacaoLibraDolar
+        moeda = "Libra"
+        moedaConvertida = "dólares"
+        
+    } else if (opcaoConversao == 3){
+         cotacao = cotacaoLibraReal
+         moeda = "Libra"
+         moedaConvertida = "reais"
+        
+    } else {
+        cotacao = "desconhecido"
+        moeda = "desconhecido"
+        moedaConvertida = "desconhecido"   
+    }
+
+    var valorParaConversao = prompt("Digite o valor a ser convertido")
+
+//alert(valorEmRealDecimal)
+
+var resultado = converteValor(valorParaConversao, cotacao)
+
+listarResultado(resultado, cotacao, moeda, moedaConvertida)
+
+
+function converteValor(valorParaConversao, cotacao){
+
+    var valorInformadaFloat = parseFloat(valorParaConversao)
+
+    var valorConvertido = valorInformadaFloat * cotacao
+    
+    var valorConvertidoArredondado = valorConvertido.toFixed(2)
+     
+    return valorConvertidoArredondado
+}
+
+function listarResultado(resultado,cotacao, moeda, moedaConvertida){
+    
+   
+    var listaResultado = document.querySelector('#listaResultado')
+    var elementoResultado = "<p> O resultado é: " + resultado + "</p>"
+        elementoResultado += "<p> Cotação do "+ moeda +" é: " + cotacao + " "+ moedaConvertida + "</p>"
+        
+        listaResultado.innerHTML = listaResultado.innerHTML + elementoResultado
+  }
+
+
+  
